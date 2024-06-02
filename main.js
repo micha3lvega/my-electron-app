57d6a50f93a3e8318462a2c4aa445c363a68a3bf
Michael Vega <micha3lvega@gmail.com>
2024-06-02 00:02:56 -0500

    Agregar logica para el cierre de la aplicacion en MacOS

--- main.js
+++ main.js
@@ -12,3 +12,14 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow();
 });
+
+/**
+ * En Windows y Linux, al salir de todas las ventanas generalmente se cierra una aplicación por completo.
+ * Para implementar esto, escucha el evento 'window-all-closed' del módulo app
+ * y llame a app.quit() si el usuario no está en macOS (darwin).
+ */
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+
